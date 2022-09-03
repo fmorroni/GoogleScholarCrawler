da26123f657f2c7ecd90b2c55d8166c689ac56d3
Franco Morroni <fmorroni99@gmail.com>
2022-09-03 04:14:43 -0300

    Fix error handling and function names

--- getArticlesURL.js
+++ getArticlesURL.js
@@ -3,45 +3,48 @@ import {Request} from 'node-fetch';
 import makeRequest from './makeRequest.js';
 import getUserId from './getUserId.js';
 
-//let userId = 'xeL-QIoAAAAJ';
+async function getArticlesURL(username) {
+    try {
+        let userId = await getUserId(username);
+        console.log(username + ': ' + userId);
+        let domain = 'https://scholar.google.com';
+        let pageStart = 0;
+        // 100 is the maximum google scholar will send for each request.
+        let pageSize = 100;
+        let citationLinks = [];
 
-async function getCitationURLs(userId) {
-    let domain = 'https://scholar.google.com';
-    let pageStart = 0;
-    // 100 is the maximum google scholar will send for each request.
-    let pageSize = 100;
-    let citationLinks = [];
+        let thereAreArticles = 1;
+        do {
+            let requestUrl = `${domain}/citations?user=${userId}&cstart=${pageStart}&pagesize=${pageSize}`;
+            console.log(pageStart, requestUrl);
+            let myRequest = new Request(requestUrl);
+            pageStart += pageSize;
 
-    let thereAreArticles = 1;
-    do {
-        let requestUrl = `${domain}/citations?user=${userId}&cstart=${pageStart}&pagesize=${pageSize}`;
-        console.log(pageStart, requestUrl);
-        let myRequest = new Request(requestUrl);
-        pageStart += pageSize;
-
-        try {
             const document = await makeRequest(myRequest);
             let citations = document.querySelectorAll('form table tbody')[0].querySelectorAll('tr');
             thereAreArticles = Boolean(citations[0].querySelector('a'));
             if (thereAreArticles) {
                 citations.forEach(ele => citationLinks.push(domain + ele.querySelector('a').href));
             } 
-        } catch (error) {
-            console.log('Error in getCitationURLs', error);
-            thereAreArticles = false;
-        }
+        } while (thereAreArticles);
 
+        return Promise.resolve(citationLinks);
+    } catch (error) {
+        return Promise.reject(console.error(new Error('Something went wrong')));
     }
-    while (thereAreArticles);
 
-    return Promise.resolve(citationLinks);
+}
+
+let username = 'Marcelo F. Frías';
+if (process.argv.length > 2) {
+    username = process.argv[2];
 }
 
 let citationLinks;
-getUserId('Marcelo F. Frías').then(userId => {
-    getCitationURLs(userId).then(response => {
-        citationLinks = response;
-        console.log("Links: ", citationLinks);
-        console.log("Amount of articles: ", citationLinks.length);
-    });
-});
+getArticlesURL(username)
+    .then(response => {
+    citationLinks = response;
+    console.log("Links: ", citationLinks);
+    console.log("Amount of articles: ", citationLinks.length);
+    })
+    .catch(error => console.error(error));
--- getUserId.js
+++ getUserId.js
@@ -1,7 +1,7 @@
 import {Request} from 'node-fetch';
 import makeRequest from './makeRequest.js';
 
-export default async function getCitationURLs(userId) {
+export default async function getUserId(userId) {
     let domain = 'https://scholar.google.com';
     let requestUrl = `${domain}/citations?view_op=search_authors&mauthors=${userId}`;
     let myRequest = new Request(requestUrl);
@@ -11,14 +11,16 @@ export default async function getCitationURLs(userId) {
         let userId = document.querySelector('a[href*="user"]:not([class])');
         if (userId) {
             return Promise.resolve(userId.href.match(/(?<=user=)[a-zA-zj0-9-]*/)[0]);
-        } 
+        } else {
+            throw new Error('No user found.');
+        }
     } catch (error) {
-        console.log('No user found.', error);
+        return Promise.reject(console.error(error));
     }
 }
 
 //let userId;
-//getCitationURLs('carlos gustavo lopez pombo').then(response => {
+//getUserId('carlos gustavo lopez pombo').then(response => {
     //userId = response;
     //console.log(userId);
 //});
