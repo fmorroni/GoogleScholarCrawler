5f94ec5a40b0b89b1324a28e7faefb80075b2c96
Franco Morroni <fmorroni99@gmail.com>
2022-09-03 01:49:03 -0300

    First commit. Add fetch and jsdom packages and getArticlesURL function

---
+++ getArticlesURL.js
@@ -0,0 +1,54 @@
+import fetch, {Request} from 'node-fetch';
+import { JSDOM } from 'jsdom';
+
+async function makeRequest(myRequest) {
+    try {
+        const response = await fetch(myRequest);
+        let document = new JSDOM(await response.text()).window.document;
+        
+        return document;
+    } catch (error) {
+        console.log('Error in makeRequest', error);
+    }
+}
+
+let userId = 'xeL-QIoAAAAJ';
+
+async function getCitationURLs(userId) {
+    let domain = 'https://scholar.google.com';
+    let pageStart = 0;
+    // 100 is the maximum google scholar will send for each request.
+    let pageSize = 100;
+    let citationLinks = [];
+
+    let thereAreArticles = 1;
+    do {
+        let requestUrl = `${domain}/citations?user=${userId}&cstart=${pageStart}&pagesize=${pageSize}`;
+        console.log(pageStart, requestUrl);
+        let myRequest = new Request(requestUrl);
+        pageStart += pageSize;
+
+        try {
+            const document = await makeRequest(myRequest);
+            let citations = document.querySelectorAll('form table tbody')[0].querySelectorAll('tr');
+            thereAreArticles = Boolean(citations[0].querySelector('a'));
+            if (thereAreArticles) {
+                citations.forEach(ele => citationLinks.push(domain + ele.querySelector('a').href));
+            } 
+        } catch (error) {
+            console.log('Error in getCitationURLs', error);
+            thereAreArticles = false;
+        }
+
+    }
+    while (thereAreArticles);
+
+    return Promise.resolve(citationLinks);
+}
+
+let citationLinks;
+getCitationURLs(userId).then(response => {
+    citationLinks = response;
+    console.log("Links: ", citationLinks);
+    console.log("Amount of articles: ", citationLinks.length);
+});
