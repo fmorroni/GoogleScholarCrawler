49485967ac11338c7dde917513f6b753e73aea55
Franco Morroni <fmorroni99@gmail.com>
2022-09-29 21:26:28 -0300

    Refactor articleParser to dynamically parse content

--- articleParser.js
+++ articleParser.js
@@ -5,11 +5,7 @@ export class Article {
     this.title;
     this.articleLink;
     this.pdfLink;
-    // this.authors;
-    // this.publicationDate;
-    // this.book;
-    // this.description;
-    // this.totalCitations;
+    this.totalCitations;
   }
 }
 
@@ -18,31 +14,20 @@ export class ArticleParser {
     this.articleDom;
     this.article;
     this.contentsNodeList;
-    this.isComplete;
     this.notFoundMsg = '<Value not found.>';
   }
 
   async generateArticle(articleUrl) {
     try {
       this.articleDom = await makeRequest(articleUrl);
-      // this.isComplete = Boolean(this.articleDom.querySelector('a[class*="title"]'));
 
       this.article = new Article();
       this.parseTitle();
-      //if (!this.isComplete) throw (new Error(`\nArticle \n--- ${article.title} ---\nat \n${articleUrl}\n is missing information. Skiping.`));
-
       this.parseArticleLink();
       this.parsePdfLink();
 
       this.contentsNodeList = this.articleDom.querySelector('div[id*="table"').querySelectorAll(':scope > div');
       this.parseContents();
-
-      // article.authors = this.parseAuthors();
-
-      // article.publicationDate = this.parsePublicationDate();
-      // article.book = this.parseBook();
-      // article.description = this.parseDescription();
-      // article.totalCitations = this.parseTotalCitations();
 
       return Promise.resolve(this.article);
     } catch (error) {
@@ -51,69 +36,38 @@ export class ArticleParser {
     }
   }
 
+  verify(value) {
+    return value ? value : this.notFoundMsg;
+  }
+
   parseTitle() {
     let title = this.articleDom.querySelector('div[id*="title"]').lastChild;
+    if (!title) title = this.articleDom.querySelector('div[id*="title"]').textContent;
 
-    this.article.title = title ? title.textContent : this.notFoundMsg;
+    this.article.title = this.verify(title.textContent);
   }
 
   parseArticleLink() {
     let articleLink = this.articleDom.querySelector('a[class*="title"]');
 
-    this.article.articleLink = articleLink ? articleLink.href : this.notFoundMsg;
+    this.article.articleLink = this.verify(articleLink.href);
   }
 
   parsePdfLink() {
     let pdfLink = this.articleDom.querySelector('div[class*="title"] > a');
 
-    this.article.pdfLink = pdfLink ? pdfLink.href : this.notFoundMsg;
+    this.article.pdfLink = this.verify(pdfLink.href);
   }
 
   parseContents() {
+    // let specialCases = new Set(['Total citations', 'Scholar articles'])
     for (let node of this.contentsNodeList) {
       let key = node.children[0].textContent;
-      this.article[key] = node.children[1].textContent;
+      if (!/total\s?citations/i.test(key) && !/scholar\s?articles/i.test(key)) {
+        this.article[key] = node.children[1].textContent;
+      } else if (/total\s?citations/i.test(key)) {
+        this.article[key] = parseInt(node.children[1].children[0].textContent.match(/\d+/));
+      }
     }
   }
-
-  // parseAuthors() {
-  //     let authors = this.contentsNodeList[0].children[1].textContent.split(', ');
-
-  //     if (!authors) authors = this.notFoundMsg;
-
-  //     return authors;
-  // }
-
-  // parsePublicationDate() {
-  //     let publicationDate = this.contentsNodeList[1].children[1].textContent;
-
-  //     if (!publicationDate) publicationDate = this.notFoundMsg;
-
-  //     return publicationDate;
-  // }
-
-  // parseBook() {
-  //     let book = this.contentsNodeList[2].children[1].textContent;
-
-  //     if (!book) book = this.notFoundMsg;
-
-  //     return book;
-  // }
-
-  // parseDescription() {
-  //     let description = this.contentsNodeList[4].children[1].textContent;
-
-  //     if (!description) description = this.notFoundMsg;
-
-  //     return description;
-  // }
-
-  //parseTotalCitations() {
-  //    let totalCitations = this.contentsNodeList[5].children[1].children[0];
-
-  //    if (!totalCitations) totalCitations = this.notFoundMsg;
-  //    //parseInt(totalCitations.textContent.match(/\d+/)[0])
-
-  //    return totalCitations;
-  //}
 }
