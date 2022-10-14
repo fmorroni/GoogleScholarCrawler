9a0d9d8a5cc98b22dbec5d2b2be9f20fb529d099
Franco Morroni <fmorroni99@gmail.com>
2022-10-14 14:56:27 -0300

    Add institution globals

--- globals.js
+++ globals.js
@@ -2,3 +2,5 @@ export const language = 'en';
 
 export const domain = 'https://scholar.google.com';
 
+export const institutionID = '16159832269951878529';
+export const institutionURL = `${domain}/citations?view_op=view_org&hl=${language}&org=${institutionID}`;
