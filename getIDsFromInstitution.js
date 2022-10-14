a4854bf26362644475e94eb6c918fd2d82607fd1
maurov <mvella@itba.edu.ar>
2022-10-13 23:35:38 -0300

    bug-fix getIDsFromInstitution.js

--- getIDsFromInstitution.js
+++ getIDsFromInstitution.js
@@ -8,30 +8,42 @@ export default async function getIDsFromInstitution(institutionURL) {
     console.log(document);
     let IDs = [];
     let buttons = document.querySelectorAll('button[type="button"]')
-    console.log(buttons);
     let button = buttons[buttons.length-1];
+    let lastPage = false;
 
-    /*while(!button.disabled) {
+    while(!lastPage) {
         
-        randDelay(10,20);
-        document = await makeRequest(getNextURL(button));
-
+        await randDelay(3,5);
+        
         let usersInPage = document.querySelectorAll('h3 > a[href*="user"]');
         
         usersInPage.forEach(user => {
-            IDs.push(user.href.match("user=([^&]*)")[1]);
+            let userID = user.href.match("user=([^&]*)")[1];
+            console.log(userID);
+            IDs.push(userID);
         });
-
-        // actualizo el boton
-        button = document.querySelectorAll('button[type="button"]')[4]
+        
+        if(!button.disabled) {    
+            // siguiente pagina
+            document = await makeRequest(getNextURL(button));
+            // actualizo el boton
+            buttons = document.querySelectorAll('button[type="button"]')
+            button = buttons[buttons.length-1];
+            console.log("Next page.")
+        }
+        else {
+            lastPage = true;
+        }
     }
-    */
 
+    console.log("Cantidad de IDs encontrados: ", IDs.length);
+    return IDs;
 }
 
 function getNextURL(button) {
-    let afterAuthor = button.outerHTML.match(/after_author\\x3d(.*?)\\/);
-    let astart = button.outerHTML.match(/astart\\x3d(.*?)'/);
+    let afterAuthor = button.outerHTML.match(/after_author\\x3d(.*?)\\/)[1];
+    let astart = button.outerHTML.match(/astart\\x3d(.*?)'/)[1];
+    console.log(astart);
     let nextURL = `${domain}/citations?view_op=view_org&hl=${language}&org=16159832269951878529&after_author=${afterAuthor}&astart=${astart}`;
     return nextURL;
 }
